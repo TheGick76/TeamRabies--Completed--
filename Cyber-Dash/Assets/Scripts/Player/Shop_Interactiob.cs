568d54fab0e8c7c642c37eca46ccceac57a2f15e
TheGick76 <mikeyc0723@gmail.com>
2025-03-13 18:46:45 -0400

    Controller support for all UI besides shop

--- Cyber-Dash/Assets/Scripts/Player/Shop_Interactiob.cs
+++ Cyber-Dash/Assets/Scripts/Player/Shop_Interactiob.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Shop_Interactiob : MonoBehaviour
 {
     public Weapon_Controller WPC;
+    public EventSystem EV;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -22,6 +24,7 @@ public class Shop_Interactiob : MonoBehaviour
     {
         if (collision.gameObject.tag == "Shop")
         {
+            EV.firstSelectedGameObject = collision.gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject;
             collision.GetComponentInChildren<Canvas>().enabled = true;
             WPC.CanFire = false;
         }
@@ -31,6 +34,7 @@ public class Shop_Interactiob : MonoBehaviour
     {
         if (collision.gameObject.tag == "Shop")
         {
+            EV.firstSelectedGameObject = null;
             collision.GetComponentInChildren<Canvas>().enabled = false;
            WPC.CanFire = true;
         }
